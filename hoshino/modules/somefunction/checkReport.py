bd030c88481a1a881bba2f93a189e7c5cf7e27d0
lzx722 <53168158+lzx722@users.noreply.github.com>
2020-08-29 16:28:02 +0800

    Update: clanrank

--- hoshino/modules/somefunction/checkReport.py
+++ hoshino/modules/somefunction/checkReport.py
@@ -12,6 +12,10 @@ sv = Service('checkReport')
 @sv.on_prefix(('报刀'))
 async def set_test(bot, ev: CQEvent):
     status = 0
+    msg1 = '警告！您报的伤害较小，请确定是否为剩余刀。如果不是剩余刀，请检查是否少报了一位数'
+    msg2 = '警告！您报的伤害较大，请检查是否多报了一位数'
+    msg3 = f'============\n此为辅助程序判断，并不影响您报刀'
+
     text = ev['message'][0]['data']['text']
     raw_message = ev['raw_message']
     if text == '':
@@ -25,13 +29,11 @@ async def set_test(bot, ev: CQEvent):
     if raw_message[0] == ' ':
         return
     if len(raw_message) < 4:
+        await bot.send(ev, f'\n{msg1}\n{msg3}',at_sender=True)
         return
     if raw_message[3] == ' ':
         return
 
-    msg1 = '警告！您报的伤害较小，请确定是否为尾刀。如果不是尾刀，请检查是否少报了一位数'
-    msg2 = '警告！您报的伤害较大，请检查是否多报了一位数'
-    msg3 = f'============\n此为辅助程序判断，并不影响您报刀'
     if report < 150000:
         await bot.send(ev, f'\n{msg1}\n{msg3}',at_sender=True)
     elif report > 1500000:
