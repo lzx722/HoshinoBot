5e84899ab6b256b80203454c9c8084fe55aea9ac
lzx722 <53168158+lzx722@users.noreply.github.com>
2021-12-22 13:41:29 +0000

    Add: 原神日历和git追踪

---
+++ hoshino/modules/github_reminder/README.md
@@ -0,0 +1,81 @@
+
+## 注意
+
+插件后续将继续在 github 更新，欢迎提交 isuue 和 request
+
+※ 提交issue反馈bug麻烦尽量提供日志谢谢，另外请勿就'如何安装插件'提问，本页面只是插件不是如何搭建机器人教程，其他可查看[提问的智慧](https://github.com/handsonic/htq)
+
+若遇到日志报“检测commits更新失败，偶尔发生属于正常现象不用慌，经常发生请反馈bug”，麻烦提issue提醒我下，万一github再改源码2333
+
+监控组织的功能后续会考虑更新，大概
+
+## 更新日志
+
+21-11-09    v1.4    跟随github源码更新修复一个小bug，(github居然偷偷改源码了23333)
+
+21-09-14    v1.3    修复string中含有其他代码引发的报错(issue #1),并增加了查仓库issue编号的判断
+
+21-09-06    v1.2    应该修复了可能进程卡死的问题
+
+21-08-27    v1.1    新增查询监控仓库列表功能，并修复接受pull requests时出现多条commits的问题
+
+21-08-23    v1.0    大概能用了？
+
+## github_reminder
+
+一个适用hoshinobot的 github仓库更新提醒 插件
+
+本插件仅供学习研究使用，插件免费，请勿用于违法商业用途，一切后果自己承担
+
+## 项目地址：
+
+https://github.com/azmiao/github_reminder
+
+## 功能
+
+```
+命令如下：
+(链接支持github镜像站)
+
+[查仓库 仓库链接] 查该链接下的commits记录
+
+[监控仓库 仓库链接] 监控该仓库，推送功能需另外开启
+
+[不要监控 仓库链接] 不再监控该仓库
+
+[查询监控仓库] 查询自己监控的仓库列表
+
+其他：
+
+(自动推送监控的仓库更新，需要手动开启)
+```
+
+
+## 简单食用教程：
+
+可看下方链接：
+
+https://www.594594.xyz/2021/08/23/github_reminder_for_hoshino/
+
+或本页面：
+
+1. 下载或git clone本插件：
+
+    在 HoshinoBot\hoshino\modules 目录下使用以下命令拉取本项目
+    ```
+    git clone https://github.com/azmiao/github_reminder
+    ```
+
+2. 安装依赖，如下载过慢请自行换源
+
+    ```
+    pip install pyyaml
+    ```
+
+3. 在 HoshinoBot\hoshino\config\ `__bot__.py` 文件的 MODULES_ON 加入 'github_reminder'
+
+    然后重启 HoshinoBot
+
+4. 在某一群内发送 “开启 github_reminder_poller” 即可启用自动推送功能
+
+    群内发送 'github推送帮助' 可获取更多帮助
---
+++ hoshino/modules/github_reminder/get_com.py
@@ -0,0 +1,104 @@
+import re
+import urllib.request
+from bs4 import BeautifulSoup
+from datetime import datetime
+from datetime import timedelta
+
+# 返回commits界面的源码
+def get_comHtml(url):
+    try:
+        req = urllib.request.Request(url)
+        webpage = urllib.request.urlopen(req, timeout=10)
+        html = webpage.read()
+    except:
+        html = '链接超时！请尝试使用镜像站或稍后尝试'
+    return html
+
+# 返回commits的信息
+def get_commits(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    link_list = []
+    n = 0
+    all_a_lable = soup.find_all('a')
+    # 去除无用的a标签
+    for a_lable in all_a_lable:
+        if 'class' not in str(a_lable):
+            all_a_lable.remove(a_lable)
+    # 对所有a标签进行检索，获取相关信息
+    for a_lable in all_a_lable:
+        if a_lable.get('class') == ['Link--primary', 'text-bold', 'js-navigation-open', 'markdown-title'] and a_lable.string != 'Merge pull request' and a_lable.string != ')':
+            # commit时的标题
+            # 判断string是否为空，为空就取text文本
+            if a_lable.string != None:
+                # 判断merge pull的情况
+                if a_lable.string.startswith('from '):
+                    com_str = 'Merge pull request: ' + a_lable.string
+                # 判断 issue的编号
+                elif a_lable.string.endswith(' ('):
+                    # print(str(a_lable))
+                    a_lable_t = str(a_lable).replace('(', '\(')
+                    a_lable_t = a_lable_t.replace('/', '\/')
+                    a_lable_temp = re.findall(r'' + a_lable_t + r'.+?<\/a>', str(html))
+                    # print(str(a_lable_temp))
+                    if a_lable_temp != []:
+                        a_lable_temp = a_lable_temp[0]
+                        a_lable_temp = a_lable_temp.replace(f'{a_lable}', '')
+                        soup_do = BeautifulSoup(str(a_lable_temp), 'html.parser')
+                        down_a_lable = soup_do.find('a')
+                        issue_str = ''
+                        if down_a_lable.get('class') == ['issue-link', 'js-issue-link']:
+                            issue_str = down_a_lable.string
+                        com_str = a_lable.string + 'issue ' + issue_str + ')'
+                # 正常情况
+                else:
+                    com_str = a_lable.string
+            # 非正常情况，可能其中有<code></code>时使用text
+            else:
+                com_str = a_lable.text
+            # commit的时间
+            p0 = a_lable.get('href')
+            other_list = re.findall(rf'{p0}.+?datetime="', str(html))
+            another_list = re.findall(rf'{p0}.+?" class="no-wrap"', str(html))
+            com_time = another_list[0].replace(f'{other_list[0]}', '')
+            com_time = com_time.replace(f'" class="no-wrap"', '')
+            priv_time = str(change_time(com_time))
+            # commit的作者
+            other_list_1 = re.findall(rf'{p0}.+?f6 color-fg-muted min-width-0">', str(html))
+            another_list_1 = re.findall(rf'{p0}.+?committed', str(html))
+            edit_html = another_list_1[0].replace(f'{other_list_1[0]}', '')
+            edit_html = edit_html.replace(f'\n\n\n  committed', '')
+            soup_tmp = BeautifulSoup(edit_html, 'html.parser')
+            for a_lable_tmp in soup_tmp.find_all('a'):
+                com_edit = a_lable_tmp.string
+            # 整理数据结构，获取前5个commit数据，n为个数减1
+            n = 4
+            if len(link_list) <= n:
+                data = {
+                    'com_str': com_str,
+                    'com_time': priv_time,
+                    'com_edit': com_edit
+                }
+                link_list.append(data)
+    return link_list, n
+
+def change_time(raw_time):
+    raw_time = str(raw_time).replace('Z', '')
+    txtfmt = raw_time[:10]+ " " + raw_time[11:19]
+    dt = datetime.strptime(txtfmt,"%Y-%m-%d %H:%M:%S")
+    priv_time = dt + timedelta(hours=8)
+    return priv_time
+
+# 生成消息文本
+def create_msg(url):
+    html = get_comHtml(url)
+    if html == '链接超时！请尝试使用镜像站或稍后尝试':
+        return html
+    link_list, n = get_commits(html)
+    msg = f'仓库{url}最近{n+1}次commit如下：'
+    for link in link_list:
+        com_time = link['com_time']
+        com_edit = link['com_edit']
+        com_str = link['com_str']
+        msg = msg + f'\n▲{com_time} {com_edit}提交了 "{com_str}"'
+    return msg
+
