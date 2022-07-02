4287cf3e795230f00361cd51b6386aedc5775e28
lzx722 <53168158+lzx722@users.noreply.github.com>
2022-07-02 20:57:13 +0800

    Fix: zyujs/clanbattle三件套

--- hoshino/modules/clanbattle_rank/rank.py
+++ hoshino/modules/clanbattle_rank/rank.py
@@ -4,23 +4,16 @@ import datetime
 import json
 import hoshino
 import re
+import calendar
 from hoshino import Service, priv 
 from hoshino.typing import CQEvent
-
-USE_ONLINE_DATA = True
 
 api_search = 'https://tools-wiki.biligame.com/pcr/getTableInfo?type=search&search={}&page=0'
 api_subsection = 'https://tools-wiki.biligame.com/pcr/getTableInfo?type=subsection'
 
 lmt = hoshino.util.FreqLimiter(10)
 
-cycle_data = {
-    'cycle_mode': 'days',
-    'cycle_days': 28,
-    'base_date': datetime.date(2020, 11, 17),  #从天蝎座开始计算
-    'base_month': 9,
-    'battle_days': 6
-}
+battle_days = 6
 
 data = {
     'subsection': {},
@@ -33,44 +26,28 @@ sv = Service('clanbattle_rank', bundle='pcr查询', help_= '查询排名 公会�
 
 boss_data = {
     "hp": [
-        [6000000, 8000000, 10000000, 12000000, 20000000],
-        [6000000, 8000000, 10000000, 12000000, 20000000],
-        [6000000, 8000000, 10000000, 12000000, 20000000],
+        [6000000, 8000000, 10000000, 12000000, 15000000],
+        [6000000, 8000000, 10000000, 12000000, 15000000],
+        [7000000, 9000000, 13000000, 15000000, 20000000],
+        [15000000, 16000000, 18000000, 19000000, 20000000],
     ],
     "rate": [
-        [1.0, 1.0, 1.3, 1.3, 1.5],  #1-3
-        [1.4, 1.4, 1.8, 1.8, 2.0],  #4-10
-        [2.0, 2.0, 2.5, 2.5, 3.0],  #11-
+        [1.2, 1.2, 1.3, 1.4, 1.5],  #1-3
+        [1.6, 1.6, 1.8, 1.9, 2.0],  #4-10
+        [2.0, 2.0, 2.4, 2.4, 2.6],  #11-34
+        [3.5, 3.5, 3.7, 3.8, 4.0],  #35-
     ],
-    "step": [0, 3, 10]
+    "step": [0, 3, 10, 34]
 }
 
 event_data = {}
 
-async def load_event_cn():
-    global event_data
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get('https://mahomaho-insight.info/cached/gameevents.json') as resp:
-                data = await resp.json()
-    except:
-        pass
-    if data and 'cn' in data:
-        event_data = None
-        now = datetime.datetime.now()
-        for item in data['cn']:
-            start_time = datetime.datetime.strptime(item['start'], r"%Y/%m/%d %H:%M")
-            end_time = datetime.datetime.strptime(item['end'], r"%Y/%m/%d %H:%M")
-            event = {'title': item['title'], 'start': start_time, 'end': end_time}
-            if '团队战' in event['title'] and end_time > now:
-                event_data = event
-        return 0
-
-    return 1
 
 def get_days_from_battle_start():
     today = datetime.date.today()
-    return (today - cycle_data['base_date']).days % cycle_data['cycle_days']
+    #today = datetime.date(2020, 8, 31)
+    month_days = calendar.monthrange(today.year,today.month)[1]
+    return today.day - (month_days - battle_days)
 
 def load_group_config(group_id):
     config_file = os.path.join(os.path.dirname(__file__), 'data', f'{group_id}.json')
@@ -390,15 +367,9 @@ async def clear_follow(bot, ev: CQEvent):
 #每日推送
 @sv.scheduled_job('cron',hour='5',minute='15')
 async def clanbattle_rank_push_daily():
-    if USE_ONLINE_DATA and await load_event_cn() == 0:
-        now = datetime.datetime.now()
-        if not event_data or now < event_data['start'] or now > event_data['end']:
-            return
-        days = (now - event_data['start']) // datetime.timedelta(days=1)
-    else:
-        days = get_days_from_battle_start()
-        if days >= cycle_data['battle_days']:
-            return
+    days = get_days_from_battle_start()
+    if days >= battle_days or days < 0:
+        return
 
     bot = hoshino.get_bot()
     group_list = get_group_list()
@@ -427,16 +398,9 @@ async def clanbattle_rank_push_daily():
 #最后一天推送 0点之后数据全部木大 所以改到最后一天23点55推送最终数据
 @sv.scheduled_job('cron',hour='23',minute='55')
 async def clanbattle_rank_push_final():
-    if USE_ONLINE_DATA and await load_event_cn() == 0:
-        now = datetime.datetime.now()
-        if not event_data or now < event_data['start'] or now > event_data['end']:
-            return
-        if event_data['end'] - now > datetime.timedelta(days=1):
-            return
-    else:
-        days = get_days_from_battle_start()
-        if days != cycle_data['battle_days'] - 1:
-            return
+    days = get_days_from_battle_start()
+    if days != battle_days - 1:
+        return
 
     bot = hoshino.get_bot()
     available_group = await sv.get_enable_groups()
